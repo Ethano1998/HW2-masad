c49c121f121a41c878426616465547638e06e740
Ethano1998 <ethan.oualid@campus.technion.ac.il>
2024-12-26 12:19:36 +0200

    start

--- Solution.py
+++ Solution.py
@@ -18,44 +18,44 @@ def create_tables() -> None:
     conn = None
     try:
         conn = Connector.DBConnector()
-        conn.execute("CREATE TABLE Customer(cust_id INTEGER NOT NULL , full_name TEXT NOT NULL,"
+        conn.execute("CREATE TABLE Customers(cust_id INTEGER NOT NULL , full_name TEXT NOT NULL,"
                      " age INTEGER NOT NULL, phone TEXT NOT NULL,"
                      "CHECK (cust_id > 0), CHECK ( age >= 18 ), CHECK ( age <= 120),"
-                     "CHECK ( LENGTH(phone) == 10 ), PRIMARY KEY(cust_id))")
-        conn.execute("CREATE TABLE Order(order_id INTEGER NOT NULL , date TIMESTAMP(0) NOT NULL,"
+                     "CHECK ( LENGTH(phone) = 10 ), PRIMARY KEY(cust_id))")
+        conn.execute("CREATE TABLE Orders(order_id INTEGER NOT NULL , date TIMESTAMP(0) NOT NULL,"
                      "delivery_fee DECIMAL NOT NULL, delivery_adress TEXT NOT NULL,"
                      "CHECK (order_id > 0), CHECK ( delivery_fee >= 0 ),"
                      "CHECK ( LENGTH(delivery_adress) >= 5 ), PRIMARY KEY(order_id))")
-        conn.execute("CREATE TABLE Dish(dish_id INTEGER NOT NULL , name TEXT NOT NULL,"
+        conn.execute("CREATE TABLE Dishes(dish_id INTEGER NOT NULL , name TEXT NOT NULL,"
                      "price DECIMAL NOT NULL, is_active BOOLEAN NOT NULL,"
                      "CHECK (dish_id > 0), CHECK ( price > 0 ),CHECK (LENGTH(name) >= 4), PRIMARY KEY(dish_id))")
         conn.execute("CREATE TABLE CustomerPlacesOrder(cust_id INTEGER,order_id INTEGER,"
                      "PRIMARY KEY(order_id),"
-                     "FOREIGN KEY (cust_id) REFERENCES Customer(cust_id) ON DELETE SET NULL,"
-                     "FOREIGN KEY (order_id) REFERENCES Order(order_id) ON DELETE CASCADE,)")
+                     "FOREIGN KEY (cust_id) REFERENCES Customers(cust_id) ON DELETE SET NULL,"
+                     "FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE)")
         conn.execute("CREATE TABLE OrderContainsDish(order_id INTEGER,dish_id INTEGER,amount INTEGER NOT NULL,price DECIMAL NOT NULL,"
                      "PRIMARY KEY(order_id, dish_id),"
-                     "FOREIGN KEY (order_id) REFERENCES Order(order_id) ON DELETE CASCADE,"
-                     "FOREIGN KEY (dish_id) REFERENCES Dish(dish_id),"
+                     "FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,"
+                     "FOREIGN KEY (dish_id) REFERENCES Dishes(dish_id),"
                      "CHECK ( amount >= 0 ))")
         conn.execute("CREATE TABLE CustomerRatedDish (cust_id INTEGER NOT NULL, dish_id INTEGER NOT NULL,"
                      "rating INTEGER NOT NULL, CHECK ( rating >= 0 ), CHECK ( rating <= 5),"
-                     "FOREIGN KEY (cust_id) REFERENCES Customer(cust_id) ON DELETE SET NULL,"
-                     "FOREIGN KEY (dish_id) REFERENCES Dish(dish_id),"
+                     "FOREIGN KEY (cust_id) REFERENCES Customers(cust_id) ON DELETE CASCADE,"
+                     "FOREIGN KEY (dish_id) REFERENCES Dishes(dish_id),"
                      "PRIMARY KEY(cust_id, dish_id))")
-        conn.execute("CREATE VIEW OrderTotalPrice AS"
+        conn.execute("CREATE VIEW OrderTotalPrice AS "
                      "SELECT od.order_id,"
-                     "SUM((od.amount * od.price) + (SELECT o.delivery_fee FROM Order o WHERE o.order_id = od.order_id)) AS total_price,"
+                     "SUM((od.amount * od.price) + (SELECT o.delivery_fee FROM Orders o WHERE o.order_id = od.order_id)) AS total_price,"
                      "(SELECT co.cust_id FROM CustomerPlacesOrder co WHERE co.order_id = od.order_id) AS cust_id"
                      "FROM OrderContainsDish od"
                      "GROUP BY od.order_id")
-        conn.execute("CREATE VIEW RatingDish AS"
+        conn.execute("CREATE VIEW RatingDish AS "
                      "SELECT dish_id,"
                      "AVG(rating) AS avg_rating"
                      "FROM CustomerRatedDish"
                      "GROUP BY dish_id")
         conn.execute("CREATE VIEW CustomerOrderedDish AS "
-                     "SELECT  CustomerPlacesOrder.customer_id, OrderContainsDish.dish_id"
+                     "SELECT  CustomerPlacesOrder.cust_id, OrderContainsDish.dish_id"
                      "FROM CustomerPlacesOrder, OrderContainsDish"
                      "WHERE CustomerPlacesOrder.order_id = OrderContainsDish.order_id")
         conn.execute("CREATE VIEW Average_profit_per_order_per_price AS "
